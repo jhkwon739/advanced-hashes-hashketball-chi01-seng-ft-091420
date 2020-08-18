c1677a0e2b5d951da6b294176c563d825393214a
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-18 02:59:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ def player_numbers(name)
   teams.each do |team|
     if game_hash[team][:team_name] == name
       binding.pry
-      team_numbers_list = game_hash[team][name][:players][:number].map { |number| number }
+      team_numbers_list = game_hash[team][:players][:number].map { |number| number }
     end
   end
   team_numbers_list
